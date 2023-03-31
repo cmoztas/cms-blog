c24263dbf28304e77f030052d7acc3f04d4faedf
cagkanoztas <cagkan.oztas@jengal.com>
2023-03-31 23:12:43 +0300

    TypeORM installed and post entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    PostModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      database: 'cms_blog',
+      username: 'root',
+      password: '12345678',
+      port: 3306,
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,25 @@
-export class Post {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  slug: string;
+
+  @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  created_on: Date;
+
+  @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  modified_on: Date;
+
+  @Column()
+  mainImageUrl: string;
+}
