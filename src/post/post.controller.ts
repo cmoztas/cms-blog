0f603f30d32bd4512bb0ab24810081f31c6909b0
cagkanoztas <cagkan.oztas@jengal.com>
2023-04-02 13:08:27 +0300

    File Upload In NestJS

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -7,7 +7,15 @@ import {
   Param,
   Delete,
   UsePipes,
-  ValidationPipe, UseInterceptors, ClassSerializerInterceptor, Req, Query, UseGuards
+  ValidationPipe,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Req,
+  Query,
+  UseGuards,
+  UploadedFile,
+  BadRequestException,
+  Res
 } from '@nestjs/common';
 import { DeleteReturn, PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -15,6 +23,9 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { Post as PostEntity } from './entities/post.entity';
 import { User } from '../auth/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { Express, Request, Response } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 interface AuthRequest extends Request {
   user: User;
@@ -46,6 +57,42 @@ export class PostController {
   @Get('/slug/:slug')
   findBySlug(@Param('slug') slug: string): Promise<PostEntity> {
     return this.postService.findBySlug(slug);
+  }
+
+  @Post('upload-photo')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req: Request, file: Express.Multer.File, cb: (error: (Error | null), filename: string) => void) => {
+        const name: string = file.originalname.split('.')[0];
+        const fileExtension: string = file.originalname.split('.')[1];
+        const newFileName = `${name.split(' ').join('_')}_${Date.now()}.${fileExtension}`;
+
+        cb(null, newFileName);
+      }
+    }),
+    fileFilter: (req: any, file, cb) => {
+      if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(null, false);
+      }
+      cb(null, true);
+    }
+  }))
+  uploadPhoto(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('File is not an image');
+    } else {
+      const response: { filePath: string } = {
+        filePath: `http://localhost:5000/posts/pictures/${file.filename}`
+      };
+
+      return response;
+    }
+  }
+
+  @Get('pictures/:filename')
+  async getPicture(@Param('filename') filename, @Res() res: Response) {
+    res.sendFile(filename, { root: './uploads' });
   }
 
   @Patch(':slug')
