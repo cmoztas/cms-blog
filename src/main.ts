a8535cd70a5a1162e93961e9666bed12d8914dfc
cagkanoztas <cagkan.oztas@jengal.com>
2023-04-03 14:56:34 +0300

    CORS fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:4300', 'http://localhost:4200'],
+    credentials: true,
   })
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
 
