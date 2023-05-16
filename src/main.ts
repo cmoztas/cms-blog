4fe8150c2893d0f611500aa0b9e878564759c484
cagkanoztas <cagkan.oztas@jengal.com>
2023-05-16 15:47:19 +0300

    cookie-parser uninstalled

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   app.enableCors({
     origin: ['http://localhost:4300', 'http://localhost:4200'],
     credentials: true,
